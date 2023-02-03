de5bfff4dfc3dce0067d15fb6c432ce477053817
oZabu <83933546+oZabu@users.noreply.github.com>
2023-02-03 22:26:03 +0900

    Update AIM.js

--- AIM.js
+++ AIM.js
@@ -4,9 +4,9 @@ function _random() {
 
 
     var H_min = 1;
-    var H_max = window_h - 70;
+    var H_max = window_h - 110;
     var W_min = 1;
-    var W_max = window_w - 70;
+    var W_max = window_w - 110;
 
     var _a = Math.floor(Math.random() * (H_max + 1 - H_min)) + H_min;
     var _b = Math.floor(Math.random() * (W_max + 1 - W_min)) + W_min;
@@ -17,7 +17,21 @@ function _random() {
     document.getElementById("_height").style.height = a;
     document.getElementById("_width").style.width = b;
 
-
 };
+function HardB() {
+    document.getElementById('_target').style.height = '30px';
+    document.getElementById('_target').style.width = '30px';
+};
+
+function NormalB() {
+    document.getElementById('_target').style.height = '60px';
+    document.getElementById('_target').style.width = '60px';
+};
+
+function EasyB() {
+    document.getElementById('_target').style.height = '90px';
+    document.getElementById('_target').style.width = '90px';
+};
+
 
 setInterval(_random, 1000);
